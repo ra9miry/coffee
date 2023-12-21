8d006fa53051106179bd7ee4aa4b931bcca76aa3
Radmiry <telmanradmir@gmail.com>
2023-12-21 21:36:37 +0600

    Initial Commit

---
+++ LatteNewLaunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LatteNewLaunch
+//
+//  Created by Радмир Тельман on 21.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
